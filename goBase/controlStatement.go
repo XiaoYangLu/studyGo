c8623d9ffbdd705dc2197568554d03540c70363a
XiaoYangLu <2421282537@qq.com>
2022-08-15 21:43:08 +0800

    基础知识

--- goBase/controlStatement.go
+++ goBase/controlStatement.go
@@ -1,1 +1,31 @@
 package goBase
+
+import "fmt"
+
+func StudyIfFor() {
+	if true {
+		fmt.Println("我是if")
+	} else if true {
+		fmt.Println("我是if else")
+	}
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("hello")
+	}
+	var str string
+	switch str {
+	case "":
+		fmt.Println("")
+	case "hi":
+		fmt.Println("hi")
+	case "hello":
+		fmt.Println("hello")
+	}
+	arr := [6]int{1, 2, 3, 4, 5, 6}
+	//for range 对arr、slice、string返回索引和值
+	//          map返回键和值
+	//			channel只返回通道内的值
+	for in, v := range arr {
+		fmt.Printf("arr[%v] 对应的值：%v", in, v)
+	}
+
+}
--- goBase/dataType.go
+++ goBase/dataType.go
@@ -5,7 +5,7 @@ import "fmt"
 /*
  * 学习go语言的数据类型
  */
-func StudyDataType()  {
+func StudyDataType() {
 	var (
 		a int       //整形
 		b float32   //浮点
@@ -15,12 +15,18 @@ func StudyDataType()  {
 		f bool      //布尔
 	)
 	fmt.Printf("%T %v \n ", a, a) //通用打印格式
-	fmt.Println(b,c,d,e,f)
+	fmt.Println(b, c, d, e, f)
 
-	//运算符：　＋，　＋＝，　－，　－＝，　＊，　＊＝，　／，　／＝，　％，　％＝，　＝＝，　！＝，　＋＋，　－－，　＞，　＜，　＞＝，　＜＝，　＞＞，　＜＜，　＞＞＝，　＜＜＝，　＆＆，　｜｜，　！
+	//运算符：　＋，　＋＝，　－，　－＝，　＊，　＊＝，　／，　／＝，　％，
+	//　％＝，　＝＝，　！＝，　＋＋，　－－，　＞，　＜，　＞＝，　＜＝，　
+	//＞＞，　＜＜，　＞＞＝，　＜＜＝，　＆＆，　｜｜，　！
 
 	//&a　指向ａ的地址
 	//*b　指向ｂ地址所对应的值
+
+	//值类型：int、float、bool、string、array这些类型都属于值类型，
+	//值类型的变量直接指向存在内存中的值，值类型的变量的值存储在栈中。
+	//引用类型：特指slice、map、channel这三种预定义类型。
 
 	//arrry、slice、map的声明格式
 	//array: var name [len]type //定长数组，type表示类型
